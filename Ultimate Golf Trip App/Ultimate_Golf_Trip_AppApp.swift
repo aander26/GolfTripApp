779dd782a0ea513eb4ec84eafc24edc240b867ed
Alex <alexanderson1985@gmail.com>
2026-02-16 15:06:04 -0500

    Initial Commit

---
+++ Ultimate Golf Trip App/Ultimate_Golf_Trip_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultimate_Golf_Trip_AppApp.swift
+//  Ultimate Golf Trip App
+//
+//  Created by Alex Anderson on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_Golf_Trip_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
